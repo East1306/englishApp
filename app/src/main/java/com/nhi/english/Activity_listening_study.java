8a62dba56acc00cf8a4a04d7b2cc0f3269134c7d
Triet <trietnguyenhoangminh@gmail.com>
2022-10-31 09:35:04 +0700

    triet-update

---
+++ app/src/main/java/com/nhi/english/Activity_listening_study.java
@@ -0,0 +1,14 @@
+package com.nhi.english;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_listening_study extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listening_study);
+    }
+}
