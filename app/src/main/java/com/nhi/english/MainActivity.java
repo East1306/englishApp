83de9c227483aa7d7ff767a6bfb2adca982249d6
Triet <trietnguyenhoangminh@gmail.com>
2022-11-04 21:37:15 +0700

    update file listen-audio

--- app/src/main/java/com/nhi/english/MainActivity.java
+++ app/src/main/java/com/nhi/english/MainActivity.java
@@ -1,95 +1,83 @@
 package com.nhi.english;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-//import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import  com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-//    ImageView google;
-//    SignInButton signInButton;
-//    GoogleSignInClient mGoogleSignInClient;
-//    static final int RC_SIGN_IN = 0;
+    ImageView google;
+    SignInButton signInButton;
+    GoogleSignInClient mGoogleSignInClient;
+    static final int RC_SIGN_IN = 0;
 
-//    FirebaseFirestore firestore;
+    FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-//        Map<String, Object> users = new HashMap<>();
-//        firestore = FirebaseFirestore.getInstance();
+        Map<String, Object> users = new HashMap<>();
+        firestore = FirebaseFirestore.getInstance();
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_home_menu);
+        setContentView(R.layout.activity_main);
 
-//        signInButton = (SignInButton) findViewById(R.id.sign_in_button);
-//        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        signInButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                switch (v.getId()) {
+                    case R.id.sign_in_button:
+                        signIn();
+                        break;
+                    // ...
+                }
+            }
+
+        });
+
+//        google = findViewById(R.id.btnLogin);
+//
+//        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
 //                .requestEmail()
 //                .build();
-//        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+//        gsc = GoogleSignIn.getClient (this,gso);
 //
-//        signInButton.setOnClickListener(new View.OnClickListener(){
+//        google.getOnClickListener(new View.OnClickListener(){
 //            @Override
-//            public void onClick(View v){
-//                switch (v.getId()) {
-//                    case R.id.sign_in_button:
-//                        signIn();
-//                        break;
-//                    // ...
-//                }
+//            public  void onClick(View view){
+//                SignIn();
 //            }
-//
 //        });
-
-//
-////        google = findViewById(R.id.btnLogin);
-//
-////        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-////                .requestEmail()
-////                .build();
-////        gsc = GoogleSignIn.getClient (this,gso);
-////
-////        google.getOnClickListener(new View.OnClickListener(){
-////            @Override
-////            public  void onClick(View view){
-////                SignIn();
-////            }
-////        });
-//    }
-//
-//    private void signIn() {
-//            Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-//            startActivityForResult(signInIntent, RC_SIGN_IN);
-//    }
-//
-//    @Override
-//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-//        if (requestCode == RC_SIGN_IN) {
-//            // The Task returned from this call is always completed, no need to attach
-//            // a listener.
-//            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-//            handleSignInResult(task);
-//        }
-//    }
-//
-//    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-//        try {
-//            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-//            Toast.makeText(this, "Sign-in-success", Toast.LENGTH_SHORT).show();
-//        } catch (ApiException e) {
-//            Log.w( "Error","signInResult:failed code=" + e.getStatusCode());
-//        }
-=======
     }
 
     private void signIn() {
-            Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-            startActivityForResult(signInIntent, RC_SIGN_IN);
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
     @Override
@@ -109,11 +97,9 @@ public class MainActivity extends AppCompatActivity {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
             Toast.makeText(this, "Sign-in-success", Toast.LENGTH_SHORT).show();
-
         } catch (ApiException e) {
             Log.w( "Error","signInResult:failed code=" + e.getStatusCode());
         }
-
     }
 
 //    private void SignIn() {
