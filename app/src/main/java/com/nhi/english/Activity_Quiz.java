32db0657303eea932253ede078cf92f78c074f7d
Triet <trietnguyenhoangminh@gmail.com>
2022-12-10 06:44:31 +0700

    update_quiz

--- app/src/main/java/com/nhi/english/Activity_Quiz.java
+++ app/src/main/java/com/nhi/english/Activity_Quiz.java
@@ -23,6 +23,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -66,8 +68,8 @@ public class Activity_Quiz extends AppCompatActivity {
 
         Intent intent_ = getIntent();
         soCau = intent_.getExtras().getInt("pos");
-
         ReadData();
+        Collections.shuffle(L);
         StartCountDown();
         Display(pos);
 
